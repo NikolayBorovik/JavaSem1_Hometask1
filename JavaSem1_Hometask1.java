a9f7066605a9110894e77f7068237da10317767f
Nikolay Borovik <nik.Borovik@bk.ru>
2023-06-08 15:19:52 +1000

    done

--- JavaSem1_Hometask1.java
+++ JavaSem1_Hometask1.java
@@ -1,22 +1,72 @@
 package Java_Seminar1.JavaSem1HomeTasks;
 
+import java.util.Scanner;
+
 public class JavaSem1_Hometask1 {
     public static void main(String[] args) {
 //         Задание
 
-// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
-// 2) Вывести все простые числа от 1 до 1000
-// 3) Реализовать простой калькулятор (+-/*)
-// 4) (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69. Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
-// Под знаком вопроса - всегда одинаковая ЦИФРА.
-// Введите уравнение: ?? + ?? = 44
-// Решение: 22 + 22 = 44
+                                    // 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! 
+                                    // (произведение чисел от 1 до n)
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your number: ");
+        // int n = sc.nextInt();
+        // int result = 0;
+        // for (int i = 1; i < n+1; i++) {
+        //     result = i * (i+1);
+        // }
+        // System.out.println(result);
+                                    // 2) Вывести все простые числа от 1 до 1000
+        // int result = 0;
+        // for (int j = 1001; j > 1; j--) {
+        //     for (int i = j-1; i > 1; i--) {
+        //         if (j%i != 0) {
+        //             result = j;
+        //         }
+        //         else {
+        //             result = 0;
+        //             break;
+        //         }
+        //     }
+        //     if (result != 0) System.out.printf("%d ", result);
+        // } 
 
-        int n = 5;
+                                    // 3) Реализовать простой калькулятор (+-/*)
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your 1st number: ");
+        var num1 = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter your 2nd number: ");
+        var num2 = Integer.parseInt(sc.nextLine());
+        System.out.print("Enter a type of operation: ");
+        char operation = sc.next().charAt(0);
+       
         int result = 0;
-        for (int i = 1; i < n+1; i++) {
-            result = i * (i+1);
+        if (operation == '*'){
+            result = num1*num2;
+            System.out.println(result);
         }
-        System.out.println(result);
+        else if (operation == '/'){
+            result = num1/num2;
+            System.out.println(result);
+        } 
+        else if (operation == '+'){
+            result = num1+num2;
+            System.out.println(result);
+        }
+        else if (operation == '-'){
+            result = num1-num2;
+            System.out.println(result);
+        }
+        else System.out.println("You have entered a wrong type of operation");
+
+        sc.close();
+
+        // 4) (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть 
+        // заменены знаком вопроса, например, 2? + ?5 = 69. Требуется восстановить выражение до верного равенства. 
+        // Предложить хотя бы одно решение или сообщить, что его нет.
+        // Под знаком вопроса - всегда одинаковая ЦИФРА.
+        // Введите уравнение: ?? + ?? = 44
+        // Решение: 22 + 22 = 44
     }
 }
